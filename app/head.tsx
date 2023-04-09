ab20c1c49cf1f51c940c1ba32a25ad6f1b1a17c2
Idan Ram <44946814+Idan91@users.noreply.github.com>
2023-04-09 21:16:34 +0300

    Update template to use app dir + dep, eslint, stylelint changes (#3)
    * Update template to use app dir + dep, eslint, stylelint changes
    
    * update deps

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+function Head() {
+  return (
+    <>
+      {/* eslint-disable-next-line i18next/no-literal-string -- Allowed for document head */}
+      <title>App</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
+
+export default Head;
---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line i18next/no-literal-string -- Temporary until app is built
+const page = () => <div>Hello world!</div>;
+
+export default page;
